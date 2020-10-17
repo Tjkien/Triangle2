91b39887441f22d293a80c567f476d56f85cfd69
Tjkien <72650836+Tjkien@users.noreply.github.com>
2020-10-17 21:49:10 +0800

    Add files via upload

--- TRI2.C
+++ TRI2.C
@@ -5,9 +5,9 @@ void main()
   int i,j, k;
   clrscr();
 
-  for(i = 0; i < 24; i++);
+  for(i = 0; i < 24; i++)
   {
-     for(j = i; j >= 0; j--);
+     for(j = 24-1; j >= i; j--)
      {
 	printf(" ");
      }
